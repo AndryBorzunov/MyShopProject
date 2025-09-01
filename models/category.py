6b3743b7629512ee63aaef44de810ddd35aa727f
Борзунов Андрей <andry73@yandex.ru>
2025-09-01 18:45:53 +0500

    private mode

--- models/category.py
+++ models/category.py
@@ -9,13 +9,29 @@ class Category:
 
     name: str                # имя категории
     description: str         # описание категории продуктов
-    products: list[Product]  # список продуктов в данной категории
+    __products: list[Product]  # список продуктов в данной категории
 
     def __init__(self, name: str, description: str, products: list[Product]):
         """ Инициализация экземпляра класса """
 
         self.name = name
         self.description = description
-        self.products = products.copy()
+        self.__products = products.copy()
         Category.category_count += 1
-        Category.product_count = len(self.products)
+        Category.product_count = len(self.__products)
+
+
+    def add_product(self, product: Product) -> None:
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.category_count += 1
+
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+
--- models/product.py
+++ models/product.py
@@ -1,16 +1,34 @@
-
+from  typing import Type
 
 class Product:
     """ Класс для представления продукта """
 
     name: str         # Наименование продукта
     description: str  # Описание продукта
-    price: float      # Стоимость
+    __price: float      # Стоимость
     quantity: int     # Количество
 
     def __init__(self, name, description, price, quantity):
         """ Метод для инициализации экземпляра класса """
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, value: float):
+        if value > 0:
+            self.__price = value
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
+
+    @classmethod
+    def new_product(cls, dict_product: dict[str, str | float | int]):
+        return cls(dict_product["name"], dict_product["description"], dict_product["price"], dict_product["quantity"])
