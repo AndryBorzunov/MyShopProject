cd30a065c22e71b1b2c493d348846d4235ba8448
Борзунов Андрей <andry73@yandex.ru>
2025-09-03 11:37:48 +0500

    lawngrass

---
+++ models/lawngrass.py
@@ -0,0 +1,16 @@
+from models.product import Product
+
+
+class LawnGrass(Product):
+
+    country: str
+    germination_period: int
+    color: str
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: int, color: str):
+        """Метод для инициализации экземпляра класса"""
+
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
---
+++ models/smartphone.py
@@ -0,0 +1,18 @@
+from  models.product import Product
+
+
+class Smartphone(Product):
+
+    efficiency: float    # производительность
+    model: str           # модель
+    memory: int          # объем встроенной памяти
+    color: str           # цвет
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: float, model: str, memory: int, color: str):
+        """Метод для инициализации экземпляра класса"""
+
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
